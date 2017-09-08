c737f416454879b8d161d6ae560d73b1b8c02c05
Lluis Suros <lluissuros@gmail.com>
2017-09-08 13:00:09 +0200

    Add simple 404 error handling

--- index.js
+++ index.js
@@ -15,8 +15,25 @@ const bookRouter = require('./src/routes/bookRoutes');
 
 app.use('/Books', bookRouter);
 // app.use('/Ratings', ratingRouter);
+//
 
-const port = 5000;
+//handle all invalid urls
+app.get('*', function(req, res, next) {
+  const err = new Error();
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  if(err.status !== 404) {
+    return next();
+  }
+
+  res.status(404);
+  res.send(err.message || '** this page doesnt exist :p **');
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log(`listening on port ${port}!`);
 });
